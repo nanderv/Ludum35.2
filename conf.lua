271925fbc86733cab5b4cda8132b07409b13fd73
nanderv <nanderv@gmail.com>
2016-04-16 20:39:17 +0200

    Fucking klotebende

--- conf.lua
+++ conf.lua
@@ -2,8 +2,13 @@ love.conf = function(t)
   t.title = "Cat"
   t.version = "0.10.1"
   t.window.width = 1200
-  t.window.height = 600
+  t.window.height = 700
   t.window.resizable = true
   t.console = true
   t.window.vsync = false
 end
+CONTROLS = {}
+CONTROLS.UP = "w"
+CONTROLS.DOWN = "s"
+CONTROLS.RIGHT = "d"
+CONTROLS.LEFT = "a"
