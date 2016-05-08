b4d72e4e87afcebb8a0d53c327f95db8dcc1052a
nanderv <nanderv@gmail.com>
2016-05-08 16:21:05 +0200

    Build script fixed.

--- build.bat
+++ build.bat
@@ -12,19 +12,23 @@ copy %game%.love 32\
 copy %game%.love 64\
 cd 32
 copy /b love.exe+%game%.love %game%.exe
+del %game%.love
+del love.exe
 cd ../64
 copy /b love.exe+%game%.love %game%.exe
+del %game%.love
+del love.exe
 cd ../
-xcopy /s 32 Windows_32\
+xcopy /s 32 %game%_32\
 jar -cMf %game%_Win_32.zip %game%_32
-xcopy /s 32 Windows_64\
+xcopy /s 32 %game%_64\
 jar -cMf %game%_Win_64.zip %game%_64
 rmdir 32 /s /Q
 rmdir 64 /s /Q
 
 
-rmdir Windows_32 /s /Q
-rmdir Windows_64 /s /Q
+rmdir %game%_32 /s /Q
+rmdir %game%_64 /s /Q
 
 
 
